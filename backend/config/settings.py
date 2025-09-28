20389bf81fb7b15cb7671b1f94d5ac7109b6316a
Andrey Udalov <109138235+mysunismine@users.noreply.github.com>
2025-09-28 20:06:43 +0400

    Fix settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -129,7 +129,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidatclearor',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
 
