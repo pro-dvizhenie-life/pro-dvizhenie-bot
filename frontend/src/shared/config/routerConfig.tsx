310df3b2ce4709d9527e0b627d03706a23abb717
Kirill Kalinin <kalinin1507.2012@gmail.com>
2025-10-04 03:42:10 +0400

    renavigation

--- frontend/src/shared/config/routerConfig.tsx
+++ frontend/src/shared/config/routerConfig.tsx
@@ -1,20 +1,19 @@
-import { MainPage } from 'src/pages/MainPage'
 import { Dashboard } from 'src/pages/Dashboard'
 import { NotFoundPage } from 'src/pages/NotFoundPage'
 import { StepPage } from 'src/pages/application/stepPage'
 
-import type { RouteProps } from 'react-router'
+import { Navigate, type RouteProps } from 'react-router'
 
 export enum AppRoutes {
 	MAIN = 'main',
-  DASHBOARD = 'dashboard',
+	DASHBOARD = 'dashboard',
 	APPLICATION = 'application',
 	NOT_FOUND = 'not_found',
 }
 
 export const RouterPath: Record<AppRoutes, string> = {
 	[AppRoutes.MAIN]: '/',
-  [AppRoutes.DASHBOARD]: '/dashboard',
+	[AppRoutes.DASHBOARD]: '/dashboard',
 	[AppRoutes.APPLICATION]: '/application',
 
 	[AppRoutes.NOT_FOUND]: '*',
@@ -23,12 +22,12 @@ export const RouterPath: Record<AppRoutes, string> = {
 export const routerConfig: Record<AppRoutes, RouteProps> = {
 	[AppRoutes.MAIN]: {
 		path: RouterPath.main,
-		element: <MainPage />,
+		element: <Navigate to={RouterPath.application} />,
 	},
-  [AppRoutes.DASHBOARD]: {
-    path: RouterPath.dashboard,
-    element: <Dashboard />,
-  },
+	[AppRoutes.DASHBOARD]: {
+		path: RouterPath.dashboard,
+		element: <Dashboard />,
+	},
 
 	[AppRoutes.APPLICATION]: {
 		path: RouterPath.application,
